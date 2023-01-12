4f3cdb25fd5b65bf2f59afb2bc787bf81a5360a4
Paweł J <jpawz@outlook.com>
2023-01-12 20:51:37 +0100

    Add basic calculations
    Only work for the first time. To do second calculation needs to refresh page.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let operator = null;
+let numberA = null;
+let numberB = null;
+
 const result = document.getElementById("result");
 
 result.textContent = "";
@@ -5,6 +9,42 @@ result.textContent = "";
 const numButtons = document.querySelectorAll(".number");
 numButtons.forEach((button) => {
   button.addEventListener("click", (b) => {
-    result.textContent = result.textContent + b.target.textContent;
+    if (operator == null) {
+      numberA = +b.target.textContent;
+      result.textContent = result.textContent + b.target.textContent;
+    } else if(operator != null && numberB == null) {
+        result.textContent = +b.target.textContent;
+        numberB = +b.target.textContent;
+    } else {
+        numberB = numberB + b.target.textContent;
+        result.textContent = result.textContent + b.target.textContent;
+    }
   });
 });
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach((button) => {
+  button.addEventListener("click", (o) => {
+    result.textContent = o.target.textContent;
+    operator = o.target.textContent;
+  });
+});
+
+const equalButton = document.getElementById("equal");
+equalButton.addEventListener("click", () => {
+  result.textContent = `${operate()}`;
+  operator = null;
+});
+
+function operate() {
+  switch (operator) {
+    case "+":
+      return numberA + numberB;
+    case "-":
+      return numberA - numberB;
+    case "x":
+      return numberA * numberB;
+    case "/":
+      return numberA / numberB;
+  }
+}
